46a1ead2e80f33a6446ea46262746d23b6ec7cb0
Moritz Angermann <moritz.angermann@gmail.com>
2021-05-18 03:18:44 +0000

    Add musl support

--- release.nix
+++ release.nix
@@ -126,7 +126,6 @@ let
     [ "cardano-node-profiled" "components" "exes" ]
     [ "checks" "tests" "cardano-node-chairman" ]
     [ "workbench" ] [ "profiles" ]
-    [ "tx-generator" ] [ "haskellPackages" "tx-generator" ] [ "exes" "tx-generator" ]
   ];
   # Paths or prefix of paths for which cross-builds (mingwW64, musl64) are disabled:
   noCrossBuild = [
